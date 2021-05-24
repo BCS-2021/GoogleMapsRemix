c82e7b3d5b0a5e748b18facf2b27a072e6d5bcc3
Christian Rhodes <rhodeschristian@icloud.com>
2021-05-23 22:41:53 -0700

    Update: Add marker to maps

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
 let map;
 
+// Displays the map itself
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 42.3601, lng: -71.0589 },
+    zoom: 13,
+  });
+
+  // puts a mark on the google maps
+  marker = new google.maps.Marker({
+    position:{lat:42.4668, lng: -70.9495},
+    map:map,
+    icon:'/images/town.png'
   });
 }
